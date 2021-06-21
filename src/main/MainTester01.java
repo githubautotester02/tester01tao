7a16cc499bb840f783aaa07a7b303d1ba9321e69
TMHoang <github.autotester02@gmail.com>
2021-06-21 23:08:33 +0700

    update

--- src/main/MainTester01.java
+++ src/main/MainTester01.java
@@ -4,7 +4,7 @@ public class MainTester01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is tester 01 !!!");
+		System.out.println("This is tester 01 !!! update nha");
 	}
 
 }
