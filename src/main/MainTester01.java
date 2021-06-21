023f266a654a3b3cba5618d397414f821ee95278
TMHoang <github.autotester02@gmail.com>
2021-06-21 23:01:54 +0700

    Init tester 01

---
+++ src/main/MainTester01.java
@@ -0,0 +1,10 @@
+package main;
+
+public class MainTester01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is tester 01 !!!");
+	}
+
+}
